d2f156d36eb5eac24a83a0f658fe3bf0961e7308
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-12-07 21:50:19 -0500

    learn nextjs 14

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <p className="mt-4 text-gray-400">Loading...</p>
+        </div>
+    );
+}
